c56d21a8781f05276ac5d8fe3ea2b0dcf98f2e08
tebibux <tebibusolomon79@gmail.com>
2023-05-17 12:59:25 +0300

    UI done

---
+++ src/Asset/ResourceObject.js
@@ -0,0 +1,89 @@
+import Gojjam from './01_Gojjam.jpg';
+import Arsi from './02_Arsi.jpg';
+import Arsi2 from './03_Arsi.jpg';
+import Tigray from './04_Tigrian.jpg';
+import Arsi3 from './05_Arsi.jpg';
+import Gonder from './06_Gonder.jpg';
+import Metihara from './07_Metihara.jpg';
+import Raya from './08_Raya.jpg';
+import Konsso from './09_Konsso.jpg';
+import Nigeria from './10_Nigerian.jpg';
+import Kenya from './11_Keniyan.jpg';
+import Uganda from './12_Uganda.jpg';
+import Gonder2 from './13_Gonder.jpg';
+import Ethiopia from './14_Ethiopian_turmi.jpg';
+import Nigeria2 from './15_Nigeria.jpg';
+import Kenya2 from './16_Keynian.jpg';
+import Jimma from './17_Jimma.jpg';
+
+const ResourceObject = {
+	gojjam: {
+		name: 'Gojjame',
+		path: Gojjam
+	},
+	arsi: {
+		name: "Arsi",
+		path: Arsi
+	},
+	arsi2: {
+		name: "Arsi 2",
+		path: Arsi2,
+	},
+	tigrian: {
+		name: "Tigray Girl",
+		path: Tigray
+	},
+	arsi3: {
+		name: "Arsi Girl",
+		path: Arsi3
+	},
+	gonder: {
+		name: "Gondere",
+		path: Gonder
+	},
+	methara: {
+		name: "Metihara",
+		path: Metihara
+	},
+	raya: {
+		name: "Raya",
+		path: Raya
+	},
+	konsso: {
+		name: "Konsso",
+		path: Konsso
+	},
+	nigeria: {
+		name: "Nigeria",
+		path: Nigeria
+	},
+	kenya: {
+		name: "Kenya",
+		path: Kenya
+	},
+	uganda: {
+		name: "Uganda", 
+		path: Uganda
+	},
+	gonder2: {
+		name: "Gonder Girl",
+		path: Gonder2
+	},
+	ethiopian: {
+		name: "Ethiopian Girl",
+		path: Ethiopia
+	},
+	nigeria2: {
+		name: "Nigeria 2",
+		path: Nigeria2
+	},
+	kenya2: {
+		name: "Kenya Girl",
+		path: Kenya2
+	},
+	jimma: {
+		name: "Jimma",
+		path: Jimma
+	}
+}
+export default ResourceObject
--- src/components/main/cardBoard/CardBoard.jsx
+++ src/components/main/cardBoard/CardBoard.jsx
@@ -1,73 +1,30 @@
 import React from 'react';
 import myImage from '../../../Asset/01_Gojjam.jpg';
-
+import myImages from '../../../Asset/ResourceObject';
 function CardBoard() {
 	return (
 		<div className='card-board'>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
-			<div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div><div className="img-box">
-				<img src={myImage} alt="" />
-				<div className="img-text">
-					<p>1</p>
-					<p>Hareri</p>
-				</div>
-			</div>
+			{
+				Object.entries(myImages).map(([key, image], index = 0) => {
+					if (index <= 8) {
+						index += 1;
+						return (
+							<div className="img-box" key={key}>
+								<img src={image.path} alt={image.name} />
+								<div className="img-text">
+									<p>{index}</p>
+									<p>{image.name}</p>
+								</div>
+							</div>
+						)
+					}
+					return null
+				})
+			}
+
 		</div>
 	);
 }
+
 
 export default CardBoard;
