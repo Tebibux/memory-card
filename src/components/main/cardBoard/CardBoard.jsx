8c97f9a35dc2702fbbd7b85ca41ab9c1e5be8d5a
tebibux <tebibusolomon79@gmail.com>
2023-05-23 22:21:04 +0300

    some change

--- src/components/main/cardBoard/CardBoard.jsx
+++ src/components/main/cardBoard/CardBoard.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import myImages from '../../../Asset/ResourceObject';
 function CardBoard() {
+	const [randomImage, setRandomImage] = useState(Object.values(myImages));
+	const [score, setScore] = useState(0);
+
+	const [existed, setExisted] = useState([]);
+
+
+	const handleRandomize = () => {
+		const shuffledImages = Object.values(myImages).sort(() => Math.random() - 0.5);
+		setRandomImage(shuffledImages);
+	}
+	const handleScore = (e) => {
+		// check if the image exits in the score array
+		// 
+		if(existed.includes(e.target.src)){
+			return
+		}
+		else{
+			setScore(score + 1)
+			setExisted([...existed, e.target.src])
+			console.log(e.target.src);
+			handleRandomize();
+		}
+		return score;
+	}
+
+
 	return (
 		<div className='card-board'>
 			{
-				Object.entries(myImages).map(([key, image], index = 0) => {
+				Object.entries(randomImage).map(([key, image], index = 0) => {
 					if (index <= 8) {
 						index += 1;
 						return (
-							<div className="img-box" key={key}>
+							<div className="img-box"
+								key={key}
+								onClick={handleScore}
+							>
 								<img src={image.path} alt={image.name} />
-								<div className="img-text">
-									<p>{index}</p>
-									<p>{image.name}</p>
-								</div>
 							</div>
 						)
 					}
@@ -24,6 +49,5 @@ function CardBoard() {
 		</div>
 	);
 }
-
 
 export default CardBoard;
--- src/components/main/cardBoard/ScoreBoard.jsx
+++ src/components/main/cardBoard/ScoreBoard.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import CardBoard from './CardBoard';
 function ScoreBoard() {
   return (
 	<div className='score-board'>
-		<h3>Your Score: 7</h3>
+		<h3>Your Score: {CardBoard.handleScore}</h3>
 		<h3>High Score: 17</h3>
 	</div>
   );
