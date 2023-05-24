96771b7e02634a42fb2e4ff019f401523e019e7a
tebibux <tebibusolomon79@gmail.com>
2023-05-24 08:18:48 +0300

    score board done

--- src/components/main/cardBoard/ScoreBoard.jsx
+++ src/components/main/cardBoard/ScoreBoard.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-function ScoreBoard({ scoreData }) {
-	const [highScore, sethighScore] = useState(scoreData)
+const ScoreBoard = ({ scoreData }) => {
+	const [highScore, setHighScore] = useState(scoreData);
+
+	useEffect(() => {
+		const storedHighScore = localStorage.getItem('highScore');
+		if (storedHighScore) {
+			setHighScore(parseInt(storedHighScore));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (scoreData > highScore) {
+			setHighScore(scoreData);
+			localStorage.setItem('highScore', scoreData);
+		}
+	}, [scoreData]);
+
 	return (
 		<div className='score-board'>
 			<h3>Your Score: {scoreData}</h3>
-			{
-				scoreData > highScore ? (
-					<h3>High Score: {highScore}</h3>
-				) : (
-					<h3>High Score: {highScore}</h3>
-					)
-			}
+			<h3>High Score: {highScore}</h3>
 		</div>
 	);
 }
-
 export default ScoreBoard;
